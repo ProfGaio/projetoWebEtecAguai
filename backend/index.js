98af29de7df89915c77af9cce3174cf634b28a77
Gaio B. Oliveira <gaio.casabranca@gmail.com>
2024-08-29 17:05:23 -0300

    Códigos para cadastro de novo produto

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const ClienteRoutes = require('./routes/ClienteRoutes')
-
+const ProdutoRoutes = require('./routes/ProdutoRoutes')
 //Configuração de resposta do JSON
 app.use(express.json())
 
@@ -13,5 +13,5 @@ app.use(express.static('public'))
 
 /* ROTAS */
 app.use('/clientes',ClienteRoutes)
-
+app.use('/produtos',ProdutoRoutes)
 app.listen(5000)

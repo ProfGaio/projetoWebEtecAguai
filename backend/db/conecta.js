9f04a5ae0fc9e1bc2e1581587d573132306ad00f
Gaio <gaio.oliveira3@etec.sp.gov.br>
2024-11-07 12:25:50 -0300

    Erros de merge corrigidos

--- backend/db/conecta.js
+++ backend/db/conecta.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 
 async function main(){
     await mongoose.connect('mongodb://localhost:27017/relogios')
-<<<<<<< HEAD
     console.log("Conectado ao MongoDB")
 }
 
@@ -10,11 +9,5 @@ main().catch((erro)=>console.log(erro))
 
 module.exports = mongoose
 
-=======
-    console.log("Conectou ao MongoDB")
-}
 
-main().catch((erro)=> console.log(erro))
 
-module.exports = mongoose
->>>>>>> meuNote
--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const ClienteRoutes = require('./routes/ClienteRoutes')
-<<<<<<< HEAD
+
 
 //Configuração de resposta do JSON
 app.use(express.json())
@@ -12,8 +12,6 @@ app.use(express.static('public'))
 
 /* HABILITAR USO DE ROTAS PELO EXPRESS */
 app.use('/clientes',ClienteRoutes)
-
-=======
 const ProdutoRoutes = require('./routes/ProdutoRoutes')
 //Configuração de resposta do JSON
 app.use(express.json())
@@ -26,5 +24,5 @@ app.use(express.static('public'))
 /* ROTAS */
 app.use('/clientes',ClienteRoutes)
 app.use('/produtos',ProdutoRoutes)
->>>>>>> meuNote
+
 app.listen(5000)
